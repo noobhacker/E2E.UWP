79fb27b4e15aa7fb9a9b1f48746706f2b979f728
Lee Song <leesong1995@hotmail.com>
2017-02-22 21:21:17 +0800

    added nextwordservice

--- E2E.UWP/WebServices/NextWordWebService.cs
+++ E2E.UWP/WebServices/NextWordWebService.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Web.Http;
 
 namespace E2E.UWP.WebServices
 {
-    public static class BreakWordWebService
+    public class NextWordWebService
     {
         static AzureHttpClient httpClient;
         const string endpoint =
-            "https://westus.api.cognitive.microsoft.com/text/weblm/v1.0/breakIntoWords?model=query&text=";
-        const string key= "cb67154570fe4d3794f0530a17c03922";
+            "https://westus.api.cognitive.microsoft.com/text/weblm/v1.0/generateNextWords?model=query&words=";
+        const string key = "cb67154570fe4d3794f0530a17c03922";
 
-        static BreakWordWebService()
+        static NextWordWebService()
         {
             httpClient = new AzureHttpClient(endpoint, key);
         }
 
-        public static async Task<string> SplitWordAsync(string input)
+        public static async Task<string> GetNextWordAsync(string input)
             => await httpClient.PostAsync<string>(input);
-
     }
 }
